a7496077aad3b69afaf4fc121874cd5a558f55f3
sistemasmgsa <sistemas@mgsa.com.pe>
2025-10-29 12:11:05 -0500

    bd

--- db/migrations/001_add_usp_oportunidades_eliminar.sql
+++
@@ -1,6 +0,0 @@
-DELIMITER $$
-CREATE DEFINER=`evento`@`%` PROCEDURE `usp_oportunidades_eliminar`(IN `in_id_oportunidad` INT)
-BEGIN
-	UPDATE oportunidades SET estado = 0 WHERE id_oportunidad = in_id_oportunidad;
-END$$
-DELIMITER ;
